9ee73fa2fdab08a3ac72a39c3a907f98efa3b6d9
Anthony McClay <anthony.mcclay@nielsen.com>
2017-06-09 06:16:03 -0400

    Changes from get to POST

--- headlines.py
+++ headlines.py
@@ -25,9 +25,10 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'aws': 'http://feeds.feedburner.com/AmazonWebServicesBlog'}
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def get_news():
-    query = request.args.get("publication");
+    # query = request.args.get("publication");
+    query = request.form.get("publication");
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
