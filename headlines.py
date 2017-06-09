e8745153fe92d1f217156690bef052e59fff71f8
Anthony McClay <anthony.mcclay@nielsen.com>
2017-06-09 05:42:56 -0400

    Initial commit

---
+++ headlines.py
@@ -0,0 +1,87 @@
+__author__ = 'anthonymcclay'
+__project__ = 'myPythonHeadlines'
+__date__ = '6/8/17'
+__revision__ = '$'
+__revision_date__ = '$'
+
+import feedparser
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+ # RSS Feed example
+
+
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
+
+
+
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'aws': 'http://feeds.feedburner.com/AmazonWebServicesBlog'}
+
+@app.route("/")
+
+@app.route("/<publication>")
+def get_publication(publication="bbc"):
+    return get_news2(publication)
+
+#
+# @app.route("/bbc")
+# def bbc():
+#     return get_news2('bbc')
+#
+#
+# @app.route("/cnn")
+# def cnn():
+#     return get_news2('cnn')
+#
+#
+# @app.route("/fox")
+# def fox():
+#     return get_news2('fox')
+#
+# @app.route("/aws")
+# def aws():
+#     return get_news2('aws')
+#
+
+def get_news2(publication):
+    feed=feedparser.parse(RSS_FEEDS[publication]);
+    #first_article = feed['entries'][0]
+    # return """<html>
+    #        <body>
+    #            <h1> RSS Headlines {3} </h1>
+    #            <b>{0}</b> <br/>
+    #            <i>{1}</i> <br/>
+    #            <p>{2}</p> <br/>
+    #        </body>
+    #    </html>
+    #    """.format(first_article.get("title"), first_article.get("published"),
+    #               first_article.get("summary"), publication);
+    return render_template("home.html",
+                           articles=feed['entries']);
+
+
+
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+        <body>
+            <h1> BBC Headlines </h1>
+            <b>{0}</b> <br/>
+            <i>{1}</i> <br/>
+            <p>{2}</p> <br/>
+        </body>
+    </html>
+    """.format(first_article.get("title"), first_article.get("published"),first_article.get("summary"))
+
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
+
